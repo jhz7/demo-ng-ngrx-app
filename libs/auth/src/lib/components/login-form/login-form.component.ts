9b8a83ed7b2962a1783c9baee38176e1141c9060
jhz7 <jhzambrano7@gmail.com>
2020-05-30 12:52:06 -0500

    Se agrega json-server para mockear peticiones a backend

---
+++ libs/auth/src/lib/components/login-form/login-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Authenticate } from '@demo-app/data-models';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+
+  @Output() submit = new  EventEmitter<Authenticate>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(authenticate: Authenticate): void {
+    this.submit.emit(authenticate);
+  }
+
+}
---
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(authenticate: any) {
+    console.log(authenticate);
+  }
+
+}
